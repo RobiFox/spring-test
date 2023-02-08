661d121dd16990a2c363cc6239b5b2c91891ae8d
Robieh <10639273+RobiFox@users.noreply.github.com>
2023-02-08 19:18:05 +0100

    added username checks

--- src/main/java/me/robi/springtest/controllers/UserController.java
+++ src/main/java/me/robi/springtest/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +25,12 @@ public class UserController {
 
     @PostMapping("/add")
     public ResponseEntity addUser(@RequestParam(value = "name") String name) {
+        if(name.length() < 3)
+            return new ResponseEntity(Collections.singletonMap("message", "User must be 3 characters or longer."), HttpStatus.UNPROCESSABLE_ENTITY);
+        if(name.length() > 16)
+            return new ResponseEntity(Collections.singletonMap("message", "User must be 16 characters or shorter."), HttpStatus.UNPROCESSABLE_ENTITY);
+        if(repositoryHolder.users.findByNameIgnoreCase(name) != null)
+            return new ResponseEntity(Collections.singletonMap("message", "User with the name already exists."), HttpStatus.CONFLICT);
         UserEntity user = new UserEntity(name);
         repositoryHolder.users.save(user);
         Map<String, Object> response = new HashMap<>();
--- src/main/java/me/robi/springtest/repositories/UserRepository.java
+++ src/main/java/me/robi/springtest/repositories/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     UserEntity findByName(String name);
+    UserEntity findByNameIgnoreCase(String name);
     long findById(long id);
 }
